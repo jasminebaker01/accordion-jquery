aeb1d53eaeca6b08faedfa9bba591de19e5f543c
jasminebaker01 <jb@cfmmedia.de>
2025-01-27 11:00:48 +0100

    refactor: accordion

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,11 @@
 $('.accordion').each(function(){
 
-    let accordionHeaders = $(this).find('.accordion-header')
-    let accordionBodys = $(this).find('.accordion-body')
-
     function toggleAccordion(index){
-        accordionBodys.eq(index).toggleClass('hidden')
+        $('.accordion-body').eq(index).toggleClass('hidden')
         $('.accordion-arrow').eq(index).toggleClass('rotate-180')
     }
 
-    accordionHeaders.each(function(index){
+    $('.accordion-header').each(function(index){
         $(this).on('click', e=> toggleAccordion(index))
     })
 })
